b9e3bd9d412b23ba76f55ddf86b1cae43ae70da2
apote10-hub <a.pote.4924@westcliff.edu>
2026-06-16 20:13:42 +0545

    Add JavaScript assignment files

---
+++ conditionals and loops/index.js
@@ -0,0 +1,10 @@
+function checkEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    console.log(number + " is an even number.");
+  } else {
+    console.log(number + " is an odd number.");
+  }
+}
+
+checkEvenOrOdd(7);
+checkEvenOrOdd(8);
